f30ad9ef0c4cafc2a8e66bc64ddb203d8e33f04a
vicky2922 <bhuvavicky10@gmail.com>
2021-09-03 20:48:08 +0530

    Initializing project

---
+++ src/main/java/com/galaxy/ecommerceconnect/ECommerceConnectApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.ecommerceconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceConnectApplication.class, args);
+	}
+
+}
